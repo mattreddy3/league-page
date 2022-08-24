2ca682487f303dcff6897ed7f13a1b7a07dfbbfd
Matthew Reddy <matt@redfigllc.com>
2022-08-24 13:16:59 +0000

    payments thru 20220824-1516

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,28 @@ export const enableBlog = true // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE
 
 /*   STEP 2   */
 export const homepageText = `
-<p>Please review the <a href="/constitution">constitution</a> before <b>August 8th</b></p>
+<p>Payments ($200) due by <b>August 28th</b></p>
+
+<h4>Paid:</h4>
+<ul>
+<li>Todd</li>
+<li>Kyle</li>
+<li>John</li>
+<li>Jason W</li>
+<li>Matt Reddy</li>
+</ul>
+<h4>Not Paid:</h4>
+<ul>
+<li>Derek</li>
+<li>Jason M</li>
+<li>Brian</li>
+<li>Sean</li>
+<li>Reece</li>
+<li>Jack</li>
+<li>Matt Reschke</li>
+</ul>
+
+<p>Any issues or feature requests on this site, please submit here: <a href="https://github.com/mattreddy3/league-page/issues">https://github.com/mattreddy3/league-page/issues</a></p>
 `
 
 /*   STEP 3   */
@@ -23,19 +44,19 @@ export const managers = [
   {
     roster: 1, // ID of the roster that the manager manages (look at the order of the power rankings graph)
     name: "Matt Reddy",
-    tookOver: 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    location: "Brooklyn", // (optional)
-    bio: "Lorem ipsum...",
+    tookOver: null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+    location: "Barcelona", // (optional)
+    bio: "Just a guy with a manbun now",
     photo: "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    fantasyStart: 2014, // (optional) when did the manager start playing fantasy football
-    favoriteTeam: "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    fantasyStart: 2005, // (optional) when did the manager start playing fantasy football
+    favoriteTeam: "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     mode: "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     rival: {
-      name: "Rival", // Can be anything (usually your rival's name)
-      link: 6, // manager array number within this array, or null to link back to all managers page
+      name: "Red Herring", // Can be anything (usually your rival's name)
+      link: 99, // manager array number within this array, or null to link back to all managers page
       image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
     },
-    favoritePlayer: 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+    favoritePlayer: 6797, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
     valuePosition: "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
     rookieOrVets: "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
     philosophy: "Your fantasy team's philosophy",
